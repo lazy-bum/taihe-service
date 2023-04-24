68ebc03da88b9cb0b3c0233814fa8655c95a148a
lazybum <lazybum_li@163.com>
2023-04-24 21:39:30 +0800

    test

--- test.js
+++ test.js
@@ -1,16 +1,45 @@
 var ModbusRTU = require("modbus-serial");
 var client = new ModbusRTU();
+const hostAddress = "192.168.31.9";
+const hostPort = { port: 503 };
 
-client.connectTCP("192.168.31.9", { port: 503 });
+async function modbusConnect() {
+  await client.connectTCP(hostAddress, hostPort)
+    .then(() => {
+      if (client.isOpen) {
+        console.log(`IP: ${hostAddress} /PORT ${hostPort.port}`);
+        pollModbusDevices(3000);
+      }
+    })
+    .catch(err => {
+      if (err.code == 'ETIMEDOUT' || err.errno == 'ECONNREFUSED') {
+        if (intervalId) {
+          clearInterval(intervalId);
+        }
+        modbusConnect();
+      }
+      console.log(err.message);
+    });
+}
 
-setInterval(function () {
-  client.readHoldingRegisters(22, 10, function (err, data) {
-    // 获取当前时间
-    //moment.locale('zh-cn');
-    console.log("----------------------------------------------------------------------");
-    console.log(data?.data);
-    console.log("----------------------------------------------------------------------");
+function pollModbusDevices(interval = 1000) {
+  intervalId = setInterval(function () {
+    for (let node of nodeIds) {
+      client.setID(node);
+      client.readHoldingRegisters(0, 1640, function (err, data) {
+        if (data) {
+          // do whatever ...
+        }
+        if (err) {
+          console.log(err.message);
+          if (err.errno == 'ECONNREFUSED') {
+            client.close();
+            modbusConnect();
+          }
+        }
+      });
+    }
+  }, interval);
+}
 
-    //console.log(data.data);
-  });
-}, 5000);
+modbusConnect()
