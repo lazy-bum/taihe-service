fe033940be969596cf8154ab87095ae69665b18f
lazybum <lazybum_li@163.com>
2023-04-24 20:53:29 +0800

    test

--- test.js
+++ test.js
@@ -1,29 +1,15 @@
-// 引入 modbus-serial 包
-const ModbusRTU = require("modbus-serial");
+const net = require('net');
+const Modbus = require('jsmodbus');
 
-// 创建一个新的 Modbus 客户端实例
-const client = new ModbusRTU();
+// 创建TCP连接
+const socket = new net.Socket();
+const client = new Modbus.client.TCP(socket, 1);
 
-// 配置串行端口
-const serialPort = "/dev/ttyUSB0"; // 串行端口，根据实际情况修改
-const serialOptions = {
-  baudRate: 9600, // 波特率
-  dataBits: 8, // 数据位
-  stopBits: 1, // 停止位
-  parity: "none" // 奇偶校验
-};
-
-// 打开串行端口并连接 Modbus 设备
-client.connectRTUBuffered(serialPort, serialOptions).then(() => {
-  // 设置设备地址
-  client.setID(1);
-
-  // 读取保持寄存器，从寄存器地址 0 开始，读取 10 个寄存器
-  client.readHoldingRegisters(0, 10).then(data => {
-    console.log("保持寄存器的数据：", data.data);
-  }).catch(error => {
-    console.error("读取保持寄存器失败：", error);
+socket.connect(503, '192.168.31.9', function () {
+  client.readInputRegisters(0, 10).then(function (resp) {
+    console.log(resp);
+    socket.destroy();
+  }).catch(function () {
+    socket.destroy();
   });
-}).catch(error => {
-  console.error("连接 Modbus 设备失败：", error);
 });
